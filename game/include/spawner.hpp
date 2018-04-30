18069b63aa79e1c306b447cc755cc3a7f3cf8d25
Nathan Hold <nathanhold@hotmail.com>
2018-05-01 01:02:54 +1000

    ADD: Template class for use for systems without types required.

--- game/include/spawner.hpp
+++ game/include/spawner.hpp
@@ -3,14 +3,13 @@
 
 #include <vector>
 #include <mutex>
+#include <nodesystem.hpp>
 
 template<typename T>
-class Spawner
+class Spawner : public NodeSystem<T>
 {
 public:
 	std::vector<T> types;
-	std::vector<T*> instances;
-	std::mutex mutex;
 
 	Spawner()
 	{
@@ -41,91 +40,6 @@ public:
 		}
 
 		return nullptr;
-	}
-
-	void DespawnBack()
-	{
-		if (instances.size() > 0)
-		{
-			auto instance = instances.back();
-			instances.pop_back();
-			delete instance;
-		}
-	}
-
-	void DespawnAll()
-	{
-		while (instances.size() > 0)
-		{
-			auto instance = instances.back();
-			instances.pop_back();
-			delete instance;
-		}
-	}
-
-	bool IsPositionOver(const sf::Vector2f &position, T** outHit)
-	{
-		for (auto it = instances.begin(); it != instances.end(); ++it)
-		{
-			if ((*it)->node.IsPositionOver(position))
-			{
-				*outHit = (*it);
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	std::vector<T*> InArea(sf::Vector2f center, float radius)
-	{
-		std::vector<T*> withinArea;
-
-		for (auto it = instances.begin(); it != instances.end(); ++it)
-		{
-			auto type = (*it);
-
-			auto vec = type->node.GetPosition() - center;
-
-			float distance = Magnitude(vec);
-			if (distance < radius)
-			{
-				withinArea.push_back(type);
-			}
-		}
-
-		return withinArea;
-	}
-
-	T* ClosestInArea(sf::Vector2f center, float radius, std::vector<T*> exclusionVec)
-	{
-		T* closest = nullptr;
-		float maxDistance = 99999;
-
-		for (auto it = instances.begin(); it != instances.end(); ++it)
-		{
-			auto type = (*it);
-
-			if (!type->node.isAlive)
-				continue;
-
-			if (std::find(exclusionVec.begin(), exclusionVec.end(), type) != exclusionVec.end())
-				continue;
-
-			auto vec = type->node.GetPosition() - center;
-			float distance = Magnitude(vec);
-
-			if (distance > radius)
-				continue;
-
-			if (distance < maxDistance)
-			{
-				closest = type;
-				maxDistance = distance;
-			}
-		}
-
-		return closest;
 	}
 };
 
